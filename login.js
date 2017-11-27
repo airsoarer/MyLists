a4af57e5168a686ebe0da512cfc08cee096c5b70
M. A. Williams <mwilliamsii233203@gmail.com>
2017-11-27 13:13:44 -0500

    Update login.js

--- login.js
+++ login.js
@@ -81,6 +81,7 @@ function stateChangedLogin(){
     firebase.auth().onAuthStateChanged(firebaseUser =>{
         if(firebaseUser){
             console.log(firebaseUser);
+            alert("working");
             location.replace("homeLists.html?room= " + firebaseUser.uid);
         }else{
             console.log("Not Logged In");
