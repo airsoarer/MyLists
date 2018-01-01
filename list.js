58bc2dcd4364dd554b71f4d41864a053abcfd808
M. A. Williams <mwilliamsii233203@gmail.com>
2017-12-31 20:38:55 -0500

    Add files via upload

--- list.js
+++ list.js
@@ -342,7 +342,7 @@ function addBack(){
                 Item:data,
             });
             ref1.remove();
-            this.parentNode.removeChild('div');
+            $(this).parent().remove();
             ref2.once('child_added', function(snapshot){
                 var data = snapshot.val();
                 var item = data.Item;
